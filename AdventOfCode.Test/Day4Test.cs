0b25d62a4832013acf4ca09bd87dd6e06724555f
dagochen <dagochen@users.noreply.github.com>
2022-12-04 14:16:53 +0100

    test: add prod test case to reveal bug FullOverlapCount

--- AdventOfCode.Test/Day4Test.cs
+++ AdventOfCode.Test/Day4Test.cs
@@ -4,18 +4,26 @@ namespace AdventOfCode.Test
 {
     public class Day4Test
     {
-        public Day4 Day4 { get; private set; }
+        public Day4 Day4WithTestData { get; private set; }
+        public Day4 Day4 { get; }
+
         public Day4Test()
         {
-            this.Day4 = new Day4(Input.Day4Test);
+            this.Day4WithTestData = new Day4(Input.Day4Test);
+            Day4WithTestData.Process();
+
+            this.Day4 = new Day4(Input.Day4);
             Day4.Process();
         }
 
         [Fact]
         public void Day4TestDataWorking()
         {
-            Day4.FullOverlapCount.ShouldBe(2);
-            Day4.PartialOverlapCount.ShouldBe(4);
+            Day4WithTestData.FullOverlapCount.ShouldBe(2);
+            Day4WithTestData.PartialOverlapCount.ShouldBe(4);
+            
+            Day4.FullOverlapCount.ShouldBe(513);
+            Day4.PartialOverlapCount.ShouldBe(878);
         }
     }
 }
